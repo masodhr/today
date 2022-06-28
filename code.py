b24dd0b4b27d93c53019ea49c6f4ca7bed0d46e6
ahmed <masod.mcr@gmail.com>
2022-06-28 11:14:32 +0530

    co

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
 from asyncore import file_dispatcher
-
-
+import linecache
+from statistics import linear_regression
+this is second linear_regression
+and this is fourth linecache
 this is my new py file_dispatcher
